80be978a48f2aad28b12089655e0505ffdebe992
yokota9248 <yokotase20@gmail.com>
2024-12-02 13:00:33 +0900

    Change HomeController to provide API response

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,4 @@
-# app/controllers/api/v1/home_controller.rb
-module Api
-  module V1
-    class HomeController < ApplicationController
-      def index
-        # ここで必要なデータを取得して返すこともできます
-        render json: { message: "Welcome to the Job Search API" }
-      end
-    end
+class HomeController < ApplicationController
+  def index
   end
 end
